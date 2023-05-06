44dcdcb86b797e2fb042e78de22fd346fdbc1ae6
Jimmy Yaguache <jyaguache1989@gmail.com>
2023-05-05 23:55:22 -0500

    Agrega información en reportes PY

--- ejercicio_python/run.py
+++ ejercicio_python/run.py
@@ -1,10 +1,13 @@
 from Universidad import Universidad
 u = Universidad("UTPL", 50, 12)
 print(u)
+u.establecer_nombre("Espe")
+print(u)
+
 
 from Carrera import Carrera
 c = Carrera("Carrera en ciencias", "Sistemas y datos", 80)
 print(c)
-
+print(c.obtener_alumnos())
 
 
